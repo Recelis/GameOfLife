28b853ebddd6bf6b6f64eeccf2dbf7bc9f1e6a5d
JackyRecelis <jackymlui@live.com>
2018-01-17 19:35:43 +1000

    redesign of page, and github link

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import Board from './components/Board'
+import Board from './components/Board';
+import github from "./img/github.png";
 
 class App extends Component {
+  openWindow(){
+    window.open("https://github.com/Recelis/gameoflife",'_blank');
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Game of Life</h1>
+        <h1 className = "title">Game of Life</h1>
         <Board/>
-        <p className= "copyRight">Created by Jacky Lui &#169; 2017</p>
+        <p className= "copyRight">Created by Jacky Lui &#169; 2017
+          <span><button onClick = {()=>this.openWindow()}className = "github"><img className = "github" src = {github}></img></button></span>
+        </p>
       </div>
     );
   }
